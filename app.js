f4c611f3db2366d1cf3541f10b8add02b5b60dee
Felicia Valentine <73612186+FeliceValentine@users.noreply.github.com>
2021-07-14 15:17:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const keypressed = document.querySelector("#kpressed");
 const keycode = document.querySelector("#kcode");
+const eventkey = document.querySelector("#eventkey")
 
 document.addEventListener("keypress", event => {
     let key = event.code;
@@ -9,5 +10,10 @@ document.addEventListener("keypress", event => {
 document.addEventListener("keypress", event => {
     let keyc = event.which;
     keycode.innerHTML = keyc;
+    
 });
 
+document.addEventListener("keypress", event => {
+    let eventK  = event.key;
+    eventkey.innerHTML = eventK;
+});
